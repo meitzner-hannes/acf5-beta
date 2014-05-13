6ba65afcd69477e6d7ea67d421958c4c8909eeea
Elliot Condon <e@elliotcondon.com>
2014-05-13 15:46:10 +1000

    minor updates before rollback testing
    Signed-off-by: Elliot Condon <e@elliotcondon.com>

--- admin/views/update-notice.php
+++ admin/views/update-notice.php
@@ -3,6 +3,9 @@
 // extract args
 extract( $args );
 $pro = false;
+$basename = 'acf-4.3.0/acf.php';
+$nonce = wp_create_nonce( 'upgrade-plugin_' . $basename );
+$nonce2 = wp_create_nonce( 'rollback-acf_' . get_option('acf_version') );
 
 ?>
 <div id="acf-upgrade-notice" class="acf-cf">
@@ -20,7 +23,7 @@ $pro = false;
 			<h2><?php _e("ACF PRO Required",'acf'); ?></h2>
 			
 			<p><?php printf(__("Thank you for updating to %s %s!", 'acf'), acf_get_setting('name'), $version ); ?><br /><?php printf(__("We have detected that this website is currently using premium add-ons (%s) which are no longer compatible. Don't panic, you can roll back to an earlier version and continue using ACF as you know it until you are ready to upgrade to ACF PRO.", 'acf'), implode(', ', $addons) ); ?></p>
-			<p><a id="acf-notice-action" href="<?php echo admin_url('update.php?action=upgrade-plugin&plugin=' . acf_get_setting('basename') . '&_wpnonce=b03242b80a'); ?>" class="acf-button blue"><?php _e("Roll back to ACF 4.3.7", 'acf'); ?></a> <a id="acf-notice-action" href="#" class="acf-button"><?php _e("Learn more about ACF PRO", 'acf'); ?></a></p>
+			<p><a id="acf-notice-action" href="<?php echo admin_url('update.php?action=upgrade-plugin&plugin=' . $basename . '&_wpnonce=' . $nonce . '&_acfrollback=' . $nonce2 ); ?>" class="acf-button blue"><?php _e("Roll back to ACF ", 'acf'); ?> <?php echo get_option('acf_version'); ?></a> <a id="acf-notice-action" href="#" class="acf-button"><?php _e("Learn more about ACF PRO", 'acf'); ?></a></p>
 			
 			<?php else: ?>	
 				
